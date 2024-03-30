8602f68da4bd6dc663f448179924e30b0eee4e05
unknown <wufoudhil@gmail.com>
2024-03-30 23:53:41 +0100

    Install, Configure and Test Celery, Redis

--- main/views.py
+++ main/views.py
@@ -1,7 +1,13 @@
+from django.http import HttpResponse
 from ninja import NinjaAPI
 from .models import Movie
+
+
+
 api = NinjaAPI()
 
 @api.get("/MovieList/")
 def MovieList(request):
     return {"movies": Movie.objects.all().order_by('ranking').values()}
+
+
--- movies/settings.py
+++ movies/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from celery.schedules import crontab
+
+import main.tasks
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'main',
-    'rest_framework',
+    'django_celery_results',
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +128,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CELERY_TIMEZONE = "Africa/Algiers"
+CELERY_TASK_TIME_LIMIT = 1 * 1
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = 'django-db'
+
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+CELERY_BEAT_SCHEDULE = {
+    "sample_task": {
+        "task": "main.tasks.sample_task",
+        "schedule": crontab(minute="*/5"),
+    },
+}
