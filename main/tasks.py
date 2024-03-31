db8232ac3b5ff2e6d621cd73881b7dd59e3d2c92
unknown <wufoudhil@gmail.com>
2024-03-31 23:59:18 +0100

    Change Celery Settings

--- main/tasks.py
+++ main/tasks.py
@@ -7,11 +7,16 @@ from .models import Movie
 logger = get_task_logger(__name__)
 
 
-@shared_task
-def increase_rank():
-    mvznmbr = Movie.objects.filter(status__in  = ['coming-up', 'starting', 'running']).values('id')
-    for m in mvznmbr:
-        old_rank = Movie.objects.filter(id=m['id']).values('ranking')[0]['ranking']
-        Movie.objects.filter(id=m['id']).update(ranking = int(old_rank+10))
-    logger.info("Movies, from creation (status='coming_up') to launch (status='running') have been ranked by 10 more.")
-    return "Done"
+@shared_task(bind=True, max_retries=3, default_retry_delay=60)
+def increase_rank(self):
+    try:
+        mvznmbr = Movie.objects.filter(status__in  = ['coming-up', 'starting', 'running']).values('id')
+        for m in mvznmbr:
+            old_rank = Movie.objects.filter(id=m['id']).values('ranking')[0]['ranking']
+            Movie.objects.filter(id=m['id']).update(ranking = int(old_rank+10))
+        logger.info("Movies, from creation (status='coming_up') to launch (status='running') have been ranked by 10 more.")
+        return "Done"
+    
+    except Exception as exc:
+        # Log the exception or handle it as needed
+        raise self.retry(exc=exc)
--- movies/settings.py
+++ movies/settings.py
@@ -147,7 +147,7 @@ CELERY_RESULT_BACKEND = 'django-db'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 CELERY_BEAT_SCHEDULE = {
-    "increase_rank": {
+    "sample_task": {
         "task": "main.tasks.increase_rank",
         "schedule": crontab(minute="*/5"),
     },
