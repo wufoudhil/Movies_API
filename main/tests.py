0929535cd83c68827750294537842cff51726e05
unknown <wufoudhil@gmail.com>
2024-03-31 02:17:39 +0100

    Pytest Movies_API

--- main/tests.py
+++ main/tests.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from .models import Movie
 from tasks import reverse_string  
 
+
 @pytest.mark.django_db
 def test_movie_create():
   m = Movie(name="Movie_Test_Name", protagonists="Movie_Test_protagonists", poster="Movie_Test_poster")
